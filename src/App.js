005e82798f6156e0ce6f465aa58fd07688c2a84e
andres-condezo <ra.condezomonge@gmail.com>
2022-02-17 10:06:39 -0500

    Create Home test and app test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import RenderCal from './pages/Cal';
@@ -8,12 +8,14 @@ import './App.css';
 
 const App = () => (
   <div id="back">
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/calculator" element={<RenderCal />} />
-      <Route path="/quote" element={<Quote />} />
-    </Routes>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<RenderCal />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </Router>
   </div>
 );
 
--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './App';
 
-it('match snapshot', () => {
+test('match app snapshot', () => {
   const tree = renderer.create(<App />).toJSON();
   expect(tree).toMatchSnapshot();
 });
