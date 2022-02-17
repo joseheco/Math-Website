91742172314e12c2857befd07f3482ca3bce779d
gdumani <gdumani@gmail.com>
2022-02-17 09:15:57 -0600

    fix label in Cal test

--- src/pages/Cal.test.js
+++ src/pages/Cal.test.js
@@ -1,7 +1,8 @@
 import { render } from '@testing-library/react';
 import React from 'react';
+import Cal from './Cal';
 
 test('Calculator page renders', () => {
-  const tree = render(<calc />);
+  const tree = render(<Cal />);
   expect(tree.toJSON).toMatchSnapshot();
 });
